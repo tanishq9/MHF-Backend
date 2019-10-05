f078c8c62a91ed9f95ca6390edb48ee2e5102414
Tanishq <tanishqsaluja18@gmail.com>
2019-10-05 19:01:49 +0530

    updated

---
+++ public/stocktaking.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    console.log('Document loaded');
+    
+    AOS.init({
+        easing: 'ease', 
+        duration: 1800,
+        once: true /* animation only happens once when the page is loaded*/
+    });
+
+});
