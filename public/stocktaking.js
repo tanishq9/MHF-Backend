42c2ab359a7fc47c48229d7cf5b46b6d64f2519e
Tanishq <tanishqsaluja18@gmail.com>
2019-10-08 02:00:04 +0530

    ..

--- public/stocktaking.js
+++ public/stocktaking.js
@@ -2,6 +2,32 @@ $(document).ready(function(){
     
     console.log('Document loaded');
     
+    var button=$('#reportbtn');
+    var amount=$('#amt');
+    var pan=$('#pannum');
+
+
+    button.click(function(){
+        console.log('Button clicked.');
+        console.log(amount.val());
+        console.log(pan.val());
+        var obj={
+            amt:amount.val(),
+            pn:pan.val()
+        };
+        if(amount.val()=='' || pan.val()==''){
+            console.log('Empty Field(s)');
+            myFunction();
+        }else{
+            $.post(
+                '/report',
+                obj,
+                function(data){
+                    console.log('Email send.');
+                }
+            )
+        }
+    });
     AOS.init({
         easing: 'ease', 
         duration: 1800,
@@ -9,3 +35,10 @@ $(document).ready(function(){
     });
 
 });
+
+function myFunction() {
+    var x = document.getElementById("snackbar");
+    x.textContent = "Empty Field(s)"
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+var rn = require('random-number');
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'mlhomefriend@gmail.com',
+        pass: 'mlhome99'
+    }
+});
 
 const app = express();
 
@@ -9,6 +19,99 @@ app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
 app.use('/',express.static(__dirname+'/public'));
+
+app.post('/report',function(req,res){
+    var amount=req.body.amt;
+    var pan_num=req.body.pn;
+    console.log(amount);
+    console.log(pan_num);
+    var gen = rn.generator({
+        min:  20
+      , max:  70
+      , integer: true
+    });
+    pan_num=gen();
+    if(amount>=9000000){
+        amount=30;
+    }
+    else if(amount<=2000000){
+        amount=0;
+    }else{
+        amount=(amount-1000000)*30;
+        amount=amount/8000000;
+    }
+    
+    console.log(pan_num);
+    console.log(amount);
+    
+    var total_score=pan_num+amount;
+    
+    if(total_score>=75){
+        var mailOptions = {
+            from: 'mlhomefriend@gmail.com',
+            to: 'tanishqsaluja18@gmail.com',
+            subject: 'ML Home Friend Report',
+            text: '',
+            html:`<p>Greeting Ram,&nbsp;</p>
+                    <p>Your credit score is ${total_score}. Our suggestions :</p>
+                    <p>1.&nbsp;</p>
+                    <p>2.</p>
+                    <p>3.</p>
+                    <p>4.</p>
+                    <p>5.</p>
+                    <p>6.</p>`
+        };
+    }
+    else if(total_score>=50 && total_score<75){
+        var mailOptions = {
+            from: 'mlhomefriend@gmail.com',
+            to: 'tanishqsaluja18@gmail.com',
+            subject: 'ML Home Friend Report',
+            text: '',
+            html:`<p>Greeting Ram,&nbsp;</p>
+            <p>Your credit score is ${total_score}. Our suggestions :</p>
+            <p>1.&nbsp;</p>
+            <p>2.</p>
+            <p>3.</p>
+            <p>4.</p>
+            <p>5.</p>`
+        };
+    }else if(total_score<50 && total_score>=25){
+        var mailOptions = {
+            from: 'mlhomefriend@gmail.com',
+            to: 'tanishqsaluja18@gmail.com',
+            subject: 'ML Home Friend Report',
+            text: '',
+            html:`<p>Greeting Ram,&nbsp;</p>
+                    <p>Your credit score is ${total_score}. Our suggestions :</p>
+                    <p>1.&nbsp;</p>
+                    <p>2.</p>
+                    <p>3.</p>
+                    <p>4.</p>`
+        };
+    }else{
+        var mailOptions = {
+            from: 'mlhomefriend@gmail.com',
+            to: 'tanishqsaluja18@gmail.com',
+            subject: 'ML Home Friend Report',
+            text: '',
+            html:`<p>Greeting Ram,&nbsp;</p>
+                    <p>Your credit score is ${total_score}. Our suggestions :</p>
+                    <p>1.&nbsp;</p>
+                    <p>2.</p>
+                    <p>3.</p>`
+        };
+    }
+
+    transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log('Email sent: ' + info.response);
+    }
+    });
+    
+});
 
 app.listen(process.env.PORT || 4444,function(){
     console.log('Server started.');
